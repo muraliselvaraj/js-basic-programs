6103e9a600dd446a990bada86c26a1a2fd7e7e5f
muraliselvaraj <msmuralicse@gmail.com>
2019-07-25 10:52:41 +0530

    find_max_continuation_ones_count problem added

---
+++ find_max_continuation_ones_count.js
@@ -0,0 +1,32 @@
+/*
+Find highest continuation of ones count in a given binary array
+
+Input:
+An array of binary integers.
+
+Expected output:
+Ex.Input: [1,0,1,1,0,1] ==> 2
+*/
+
+let input = [1,0,1,1,0,1,1,1,1,0];
+
+const findMaxContinuesOnesCount = (arr) => {
+	if (arr.length == 0) return false;
+	let temp_count = 0, max_count = 0;
+	let idx = 0;
+	while (idx < arr.length) {
+		if (arr[idx] == 1) {
+			temp_count++;
+			if (temp_count > max_count) {
+				max_count = temp_count
+			}
+		} else {
+			temp_count = 0
+		}
+		idx++;
+	}
+	return max_count
+}
+
+var max_ones_count = findMaxContinuesOnes(input);
+console.log("max_ones_count: ", max_ones_count)
